0e19549c8feb2bcba82ee2ba66ec2828314bf8a6
KimberlyPeters <kimberlypeters468@gmail.com>
2023-04-17 01:23:44 -0700

    Getline

--- shell.h
+++ shell.h
@@ -24,10 +24,16 @@ int _execute(char **args, char *input);
 int _launch(char **args);
 void sigint_handler(int sig); 
 
-int _strcspn(char *str, const char *delimiter)
-int _strspn(char *str, const char *delimiter)
-char *_strtok_r(char *str, const char *delimiter, char **save_str)
-char *_strtok(char *str, const char *delimiter)
+/* Function declarations (prototypes) for auxiliary funtions */
+int alloc_error(char *buffer);
+ssize_t _getline(char **line, size_t *n, __attribute__((unused)) FILE *stream);
+char *realloc_buffer(char *buffer, size_t *bufsize);
+int check_alloc(void *ptr);
+
+int _strcspn(char *str, const char *delimiter);
+int _strspn(char *str, const char *delimiter);
+char *_strtok_r(char *str, const char *delimiter, char **save_str);
+char *_strtok(char *str, const char *delimiter);
 
 
 	
