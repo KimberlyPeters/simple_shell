91e0b41188072a0fa8d11555f2e07018b0124327
KimberlyPeters <kimberlypeters468@gmail.com>
2023-04-26 10:30:35 -0700

    Str aux

--- shell.h
+++ shell.h
@@ -72,7 +72,7 @@ void _memcpy(void *dest, const void *src, unsigned int n);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char **_reallocdp(char **ptr, unsigned int old_size, unsigned int new_size);
 
-int _strlen(char *s);
+int _strlen(const char *s);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *str1, char *str2);
 char *_strcpy(char *dest, char *src);
